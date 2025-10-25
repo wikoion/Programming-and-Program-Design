992c84379618c9e3c86b0516b267dd9f5968b8a6
James Gore <83005220+thejamesgore@users.noreply.github.com>
2025-10-25 20:53:25 +0100

    refactor: simplyfy factorial function

--- src/Main.kt
+++ src/Main.kt
@@ -6,10 +6,6 @@ fun main() {
         require(n2 >= 0){ "Second must be non-negative" }
         require(n3 >= 0){ "Third must be non-negative" }
 
-        var res1 = n1
-        var res2 = n2
-        var res3 = n3
-
         fun calcFactorial(n: Int):Long {
             var result = 1L
             for(i in 1..n){
@@ -17,7 +13,9 @@ fun main() {
             }
             return result
         }
-        return calcFactorial(res1) + calcFactorial(res2) + calcFactorial(res3)
-
+        return calcFactorial(n1) + calcFactorial(n2) + calcFactorial(n3)
     }
+
+    println("${factorial(1,2,5)}")
+
 }
