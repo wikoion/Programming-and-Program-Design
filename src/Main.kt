4a004429492790d6e7671ddad99f2e47e3fba5d2
James Gore <83005220+thejamesgore@users.noreply.github.com>
2025-10-25 21:01:16 +0100

    fix: Handling of larger inputs, prevent overflow, by using BigInteger type

--- src/Main.kt
+++ src/Main.kt
@@ -1,21 +1,23 @@
+import java.math.BigInteger
+
 fun main() {
     println("Change ME")
 
-    fun factorial(n1: Int, n2: Int, n3:Int):Long {
+    fun factorial(n1: Int, n2: Int, n3:Int):BigInteger {
         require(n1 >= 0) { "First number must be non-negative" }
         require(n2 >= 0){ "Second must be non-negative" }
         require(n3 >= 0){ "Third must be non-negative" }
 
-        fun calcFactorial(n: Int):Long {
-            var result = 1L
-            for(i in 1..n){
-                result *= i
+        fun calcFactorial(n: Int):BigInteger {
+            var result = BigInteger.ONE
+            for (i in 1..n) {
+                result = result.multiply(BigInteger.valueOf(i.toLong()))
             }
             return result
         }
         return calcFactorial(n1) + calcFactorial(n2) + calcFactorial(n3)
     }
 
-    println("${factorial(1,2,5)}")
+//    println("${factorial(1,2,3)}")
 
 }
