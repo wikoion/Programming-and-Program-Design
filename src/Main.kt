8cb1a7945f42c23744f91fd8ca2bd173acc514da
James Gore <83005220+thejamesgore@users.noreply.github.com>
2025-11-12 00:08:10 +0000

    Fix handleFactorialCommand

--- src/Main.kt
+++ src/Main.kt
@@ -46,17 +46,45 @@ fun handleConversionCommand(){
     TODO()
 }
 
-fun handleFactorialCommand(n1: Int, n2: Int, n3:Int):BigInteger {
-        require(n1 >= 0) { "First number must be non-negative" }
-        require(n2 >= 0){ "Second must be non-negative" }
-        require(n3 >= 0){ "Third must be non-negative" }
+fun handleFactorialCommand() {
+    println("Enter three whole numbers separated by commas e.g.: 3, 4, 5):")
+    val input = readLine()
 
-        fun calcFactorial(n: Int):BigInteger {
-            var result = BigInteger.ONE
-            for (i in 1..n) {
-                result = result.multiply(BigInteger.valueOf(i.toLong()))
-            }
-            return result
-        }
-        return calcFactorial(n1) + calcFactorial(n2) + calcFactorial(n3)
+    if (input.isNullOrBlank()) {
+        println("Error: you need to enter some numbers.")
+        return
     }
+
+    val numbers = input.trim().split(", ")
+    if (numbers.size != 3) {
+        println("Error: please enter exactly three numbers separated by commas")
+        return
+    }
+
+    val n1 = numbers[0].toIntOrNull()
+    val n2 = numbers[1].toIntOrNull()
+    val n3 = numbers[2].toIntOrNull()
+
+    if (n1 == null || n2 == null || n3 == null || n1 < 0 || n2 < 0 || n3 < 0) {
+        println("Error: all numbers must be non-negative integers")
+        return
+    }
+
+    val result = factorial(n1, n2, n3)
+    println("The sum of factorials is: $result")
+}
+
+fun factorial(n1: Int, n2: Int, n3:Int):BigInteger {
+    require(n1 >= 0) { "First number must be non-negative" }
+    require(n2 >= 0){ "Second must be non-negative" }
+    require(n3 >= 0){ "Third must be non-negative" }
+
+    fun calcFactorial(n: Int):BigInteger {
+        var result = BigInteger.ONE
+        for (i in 1..n) {
+            result = result.multiply(BigInteger.valueOf(i.toLong()))
+        }
+        return result
+    }
+    return calcFactorial(n1) + calcFactorial(n2) + calcFactorial(n3)
+}
